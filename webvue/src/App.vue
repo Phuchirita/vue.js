ac3d9b2d6365ea89db0fe077ff06e8c6ea5051e8
Phuchirita <phuchirita.b@gmail.com>
2022-03-13 16:41:23 +0700

    Editfile

---
+++ webvue/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <todo-list />
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodoList
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  height: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 3rem;
+  padding: 0 2rem;
+  background: #38ada9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
+  height: 100%;
+}
+h1 {
+  margin: 0;
+  padding: 0;
+  color: #f6b93b;
+}
+
+ul {
+  margin: 0;
+}
+</style>
